38278927ce900b5869e3a5744825647281cdd766
SeungMyeong <seungmyeongchoi@neptunecompany.kr>
2026-02-10 15:06:57 +0900

    chore: update v1.0.3 -> v1.0.4

--- pointpub_sdk/lib/pointpub_sdk_version.dart
+++ pointpub_sdk/lib/pointpub_sdk_version.dart
@@ -1,7 +1,7 @@
 final class PointPubSDKVersion {
   PointPubSDKVersion._();
 
-  static const String plugin = "1.0.3";
+  static const String plugin = "1.0.4";
   static const String android = "2.0.4";
-  static const String ios = "2.0.3";
+  static const String ios = "2.0.4";
 }
